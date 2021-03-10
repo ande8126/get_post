85c6056e013e54fbdbb9e5655a095ce56df4eacc
Pat Anderson <patrick.b.anderson@gmail.com>
2021-03-10 14:36:27 -0600

    added ajax and try-then-catch chain to test server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,26 @@
-console.log( 'JS ');
 
 $(document).ready(onReady)
 
 function onReady(){
-    console.log( 'JQ' );
+    getMessages();
 }//end onReady
+
+function getMessages(){
+    //make an AJAX GET request to server for messages
+    //try-then-catch chain:
+    $.ajax({
+        type: 'GET',
+        url:'/messages'
+    }).then( function( response ){
+        //if successful, show on dom
+        console.log( 'back from the server with:', response );
+    }).catch( function( err ){
+        //catch any errors
+        //display erroors in consle
+        alert( 'nope' );
+        console.log( err );
+    })//end AJAX
+    //if successful, show on the DOM
+    //catch any errors
+    //display errors in the console
+}
