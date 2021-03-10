58a2248e58b07e3e274011e33c05f4b149d4f94a
Pat Anderson <patrick.b.anderson@gmail.com>
2021-03-10 15:03:14 -0600

    added sendMessage function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,8 +2,12 @@
 $(document).ready(onReady)
 
 function onReady(){
+    //make click handlers
+    $( '#sendMessageButton' ).on( 'click', sendMessage );
+    //initialization
     getMessages();
 }//end onReady
+
 
 function getMessages(){
     //make an AJAX GET request to server for messages
@@ -23,4 +27,18 @@ function getMessages(){
     //if successful, show on the DOM
     //catch any errors
     //display errors in the console
-}
+}//end getMessages
+
+function sendMessage(){
+    //get user input & place it in an object
+    let objectToSend = {
+        author: $( '#nameIn' ).val(),
+        body: $( '#messageIn' ).val()
+    }//end objectToSend
+    console.log( 'sending message:', objectToSend);
+    //send the object to the server via AJAX POST
+    //if successful, update DOM
+    //catch any errors
+    // empty the messageIn element
+    $( '#messageIn' ).val( '' );
+}//end sendMessage
